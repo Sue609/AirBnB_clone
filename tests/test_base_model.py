c345328ad6cd7cb63c403c2842df8012fe15e93e
Sue609 <susankamau609@gmail.com>
2023-07-14 06:27:09 -0700

    Unittest for files, classes, functions must be tested with unit tests. They are nboth in interactive and non interactive mode.

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,14 +1,81 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
+import unittest
+from datetime import datetime
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+class TestBaseModel(unittest.TestCase):
+    def setUp(self):
+        self.base_model = BaseModel()
+
+    def test_object_creation(self):
+        my_model = BaseModel()
+        self.assertIsInstance(my_model.id, str)
+        self.assertIsInstance(my_model.created_at, datetime)
+        self.assertIsInstance(my_model.updated_at, datetime)
+
+    def test_str_method(self):
+        my_model = BaseModel()
+        my_model.name = "Test Model"
+        my_model.my_number = 42
+        expected_output = f"[BaseModel] ({my_model.id}) {my_model.to_dict()}"
+        self.assertEqual(str(my_model), expected_output)
+
+    def test_save_method(self):
+        my_model = BaseModel()
+        my_model.save()
+        self.assertIsInstance(my_model.updated_at, datetime)
+
+    def test_to_dict_method(self):
+        my_model = BaseModel()
+        my_model.name = "Test Model"
+        my_model.name = "Test Model"
+        my_model.my_number = 42
+        my_model_dict = my_model.to_dict()
+        self.assertIsInstance(my_model_dict, dict)
+        self.assertIn('id', my_model_dict)
+        self.assertIn('created_at', my_model_dict)
+        self.assertIn('updated_at', my_model_dict)
+        self.assertIn('__class__', my_model_dict)
+        self.assertIn('name', my_model_dict)
+        self.assertIn('my_number', my_model_dict)
+        self.assertEqual(my_model_dict['name'], "Test Model")
+        self.assertEqual(my_model_dict['my_number'], 42)
+
+    def test_attribute_arguement(self):
+        my_model = BaseModel()
+        my_model.name = "Test Model"
+        my_model.my_number = 42
+        self.assertEqual(my_model.name, "Test Model")
+        self.assertEqual(my_model.my_number, 42)
+
+    def test_attribute_modification(self):
+        my_model = BaseModel()
+        my_model.name = "Test Model"
+        my_model.my_number = 42
+        my_model.name = "Updated Model"
+        my_model.my_number = 24
+        self.assertEqual(my_model.name, "Updated Model")
+        self.assertEqual(my_model.my_number, 24)
+
+    def test_attribute_deletion(self):
+        my_model = BaseModel()
+        my_model.name = "Test Model"
+        my_model.my_number = 42
+        del my_model.name
+        self.assertFalse(hasattr(my_model, "name"))
+
+    def test_serialization_deserialization(self):
+        my_model = BaseModel()
+        my_model.name = "Test Model"
+        my_model.my_number = 42
+
+        serialized_data = my_model.to_dict()
+        deserialized_model = BaseModel(**serialized_data)
+
+        self.assertEqual(my_model.id, deserialized_model.id)
+        self.assertEqual(my_model.name, deserialized_model.name)
+        self.assertEqual(my_model.my_number, deserialized_model.my_number)
+
+
+if __name__ == '__main__':
+    unittest.main

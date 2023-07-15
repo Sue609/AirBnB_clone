ef4626860899afd503bd4c53fb68c672b1e6bb1d
Sue609 <susankamau609@gmail.com>
2023-07-15 10:32:53 -0700

    Updating the -base_model.py- file to adhere to the pycode style. By removing the extra white spaces and reducing the number of characters in a single line by splitting the line.

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-    The base_model module of the AirBnB webapp
-    It defines the classes that makes up the web App's logic
+The base_model module of the AirBnB webapp
 """
 from datetime import datetime
 import uuid
@@ -10,7 +9,7 @@ from models import storage
 
 class BaseModel:
     """
-        BaseModel the Main class from which all other classes will inherit
+    BaseModel the Main class from which all other classes will inherit
     """
     def __init__(self, *args, **kwargs):
         """
@@ -19,42 +18,42 @@ class BaseModel:
             *args: Variable length of arguement list.
             **kwargs: Arbitrary keyword arguements.
         """
-        if (len(kwargs) == 0):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        value = datetime.strptime(
+                                value, "%Y-%m-%dT%H:%M:%S.%f"
+                        )
+                    setattr(self, key, value)
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
-        else:
-            for key, value in kwargs.items():
-                if (key == '__class__'):
-                    continue
-                elif key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
-                else:
-                    setattr(self, key, value)
-
 
     def __str__(self):
         """
         Prints and returns attributes of a given class
         """
-        return ('[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.to_dict()))
-
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__, self.id, self.__dict__
+        )
 
     def save(self):
         """
-        Updates public instances of a class and saves it to file
+         Updates the public instance attribute 'updated_at'
+         with the current datetime.
         """
         self.updated_at = datetime.now()
         storage.save()
-
 
     def to_dict(self):
         """
         Returns dict representation of a class instance
         """
         result = self.__dict__.copy()
-        result['__class__'] =  self.__class__.__name__
+        result['__class__'] = self.__class__.__name__
         result['updated_at'] = self.updated_at.isoformat()
         result['created_at'] = self.created_at.isoformat()
         return (result)

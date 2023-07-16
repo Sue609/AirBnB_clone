a6553bd889b245e987f9638f894f2e78c8cca477
MarB-T <danmarbet@gmail.com>
2023-07-16 13:59:52 +0300

    Undo changes; not working

--- models/base_model.py
+++ models/base_model.py
@@ -18,9 +18,6 @@ class BaseModel:
             *args: Variable length of arguement list.
             **kwargs: Arbitrary keyword arguements.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
         if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
@@ -30,6 +27,9 @@ class BaseModel:
                         )
                     setattr(self, key, value)
         else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             storage.new(self)
 
     def __str__(self):

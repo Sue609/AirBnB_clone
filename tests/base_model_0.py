9488116246cf8fa1a8f7382572a4d8cc86a1744f
Sue609 <susankamau609@gmail.com>
2023-07-17 00:01:39 -0700

    More unittests to test the save, created_at, __str__ and to_dict methods found in base_model.py.

---
+++ tests/base_model_0.py
@@ -0,0 +1,12 @@
+import unittest
+from models.base_model import BaseModel
+
+class TestSaveMethod(unittest.TestCase):
+    def test_save(self):
+        bm = BaseModel()
+        bm.save()
+        self.assertIsNotNone(bm.updated_at)
+
+
+if __name__ == '__main__':
+    unittest.main()
